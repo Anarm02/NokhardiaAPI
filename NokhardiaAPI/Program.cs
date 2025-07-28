7edd97576e8fb6bd62126edc9aac707cb0c62209
Anar_Mammadli <mammadli.anar2002@gmail.com>
2025-07-28 12:16:47 +0400

    commit

--- NokhardiaAPI/Program.cs
+++ NokhardiaAPI/Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 using Microsoft.EntityFrameworkCore;
 using NokhardiaAPI.Context;
 using NokhardiaAPI.DTO_s;
@@ -25,9 +25,12 @@ namespace NokhardiaAPI
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+			var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+			app.Urls.Add($"http://*:{port}");
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+			app.MapGet("/", () => "API işləyir!");
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
